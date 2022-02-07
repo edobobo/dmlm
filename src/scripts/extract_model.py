5296aa256f41e509ee0a27e93fb26ce750ac9da7
edobobo <Pisellino28>
2022-02-07 15:00:56 +0100

    implemented a script to extract transformer models from pl_modules

---
+++ src/scripts/extract_model.py
@@ -0,0 +1,45 @@
+import os
+from typing import Optional
+
+import argparse
+
+from src.pl_modules import BERTDMLM
+
+
+def extract_transformer(
+    model_path: str, output_dir: str, member_variable: Optional[str] = None
+) -> None:
+    pl_module = BERTDMLM.load_from_checkpoint(model_path)
+
+    if member_variable is None:
+        member_variable = "bert_model"
+
+    transformer_model = getattr(pl_module, member_variable, None)
+
+    if transformer_model is None:
+        print(
+            f"The member variable '{member_variable}' does not exist in the provided model_path {model_path}"
+        )
+
+    transformer_model.save_pretrained(output_dir)
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path", required=True)
+    parser.add_argument("--output-dir", required=True)
+    parser.add_argument("--member-variable")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    if os.path.isdir(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    extract_transformer(args.model_path, args.output_dir, args.member_variable)
+
+
+if __name__ == "__main__":
+    main()
