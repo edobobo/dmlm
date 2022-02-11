a07cef95b675969d6b60db5e66aa89587700288a
edobobo <edoardo.barba94@gmail.com>
2022-02-11 18:29:25 +0100

    efficient dataset implementation

--- src/dmlm_dataset.py
+++ src/dmlm_dataset.py
@@ -2,6 +2,8 @@ from collections import Counter, defaultdict
 from functools import lru_cache
 from typing import List, Dict, Any, Optional, Tuple
 
+import datasets
+from datasets import Features
 from torch.utils.data.dataset import T_co
 from tqdm import tqdm
 
@@ -475,20 +477,336 @@ class DMLMDataset(MLMDataset):
         return len(self.final_dataset)
 
 
+def offsets_to_word2tokens(example):
+    word2tokens = list()
+    first_start = -1
+    last_end = -1
+    last_bpe_inserted = 0
+    for i, (start, end) in enumerate(example["offset_mapping"]):
+
+        if first_start == -1:
+            first_start = start
+            last_end = end
+        elif start == last_end:
+            last_end = end
+        else:
+            word2tokens.append((last_bpe_inserted, i))
+            last_bpe_inserted = i
+            first_start = start
+            last_end = end
+
+        if i == len(example["offset_mapping"]) - 1:
+            word2tokens.append((last_bpe_inserted, i + 1))
+
+    example["word2tokens"] = word2tokens
+
+    return example
+
+
+class EfficientDMLMDataset(MLMDataset):
+    def __init__(
+        self,
+        dataset_path: str,
+        inventories: Dict[str, SenseInventory],
+        transformer_model: str,
+        defined_special_token: str,
+        definition_special_token: str,
+        mlm_probability: float,
+        plain_mlm_probability: float = 0.0,
+    ):
+        super().__init__(
+            transformer_model,
+            mlm_probability,
+            additional_special_tokens=[defined_special_token, definition_special_token],
+        )
+
+        self.logger = logging.getLogger(EfficientDMLMDataset.__name__)
+
+        self.defined_special_token = defined_special_token
+        self.definition_special_token = definition_special_token
+
+        self.dataset_path = dataset_path
+        self.inventories = inventories
+        self.plain_mlm_probability = plain_mlm_probability
+
+        self.dataset_store = None
+        self.sense_inverse_frequencies = None
+
+        self._load_dataset()
+        self._clean_dataset()
+        self._tokenize_dataset()
+        self._load_sense_inverse_frequencies()
+
+    def _load_dataset(self):
+        self.logger.info("Loading dataset")
+        self.dataset_store = datasets.load_dataset(
+            "json",
+            data_files=self.dataset_path,
+            features=Features(
+                {
+                    "sentence": datasets.Value("string"),
+                    "labels": datasets.Sequence(datasets.Value("string")),
+                    "dataset_id": datasets.Value("string"),
+                }
+            ),
+        )["train"]
+
+    def _clean_dataset(self):
+        self.logger.info("Cleaning dataset")
+        self.dataset_store = self.dataset_store.filter(
+            lambda example: all(token is not None for token in example["sentence"])
+        )
+
+    def _tokenize_dataset(self):
+        self.logger.info("Tokenizing dataset")
+        self.dataset_store = self.dataset_store.map(
+            lambda examples: self.tokenizer(
+                examples["sentence"],
+                return_token_type_ids=False,
+                return_attention_mask=False,
+                return_offsets_mapping=True,
+                add_special_tokens=False,
+            ),
+            batched=True,
+        )
+
+        self.dataset_store = self.dataset_store.filter(
+            lambda examples: len(examples["input_ids"])
+            < self.tokenizer.model_max_length
+        )
+
+        self.dataset_store = self.dataset_store.map(offsets_to_word2tokens)
+
+    def _load_sense_inverse_frequencies(self):
+        self.logger.info("Computing senses inverse frequencies")
+        inventory2sense_count: Dict[str, Counter] = defaultdict(Counter)
+
+        for sample in self.dataset_store:
+            inventory2sense_count[sample["dataset_id"]].update(
+                _l for _l in sample["labels"] if _l is not None
+            )
+
+        self.sense_inverse_frequencies = dict()
+        for sense_count in inventory2sense_count.values():
+            total_occ = sum(sense_count.values())
+            for sense, count in sense_count.items():
+                self.sense_inverse_frequencies[sense] = 1 / (
+                    count / total_occ
+                )  # inverse doc freq
+
+    def __getitem__(self, item):
+        return self.dataset_store[item]
+
+    def __len__(self):
+        return len(self.dataset_store)
+
+    @lru_cache(maxsize=100_000)
+    def _tokenize_definition(self, definition: str) -> List[int]:
+        return self.tokenizer(definition, add_special_tokens=False).input_ids
+
+    def _torch_mask_tokens(
+        self,
+        inputs: torch.Tensor,
+        labels: torch.Tensor,
+        special_tokens_mask: Optional[Any] = None,
+    ) -> Tuple[Any, Any]:
+        """
+        Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
+        """
+
+        # We sample a few tokens in each sequence for MLM training (with probability `self.mlm_probability`)
+        probability_matrix = torch.full(labels.shape, self.mlm_probability)
+        if special_tokens_mask is None:
+            special_tokens_mask = torch.tensor(
+                [
+                    self.tokenizer.get_special_tokens_mask(
+                        _input, already_has_special_tokens=True
+                    )
+                    for _input in inputs
+                ],
+                dtype=torch.bool,
+            )
+        else:
+            special_tokens_mask = special_tokens_mask.bool()
+
+        probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        defined_token_id = self.tokenizer.convert_tokens_to_ids(
+            self.defined_special_token
+        )
+        defined_token_mask = inputs == defined_token_id
+        ignored_indices = torch.logical_and(~masked_indices, ~defined_token_mask)
+        labels[ignored_indices] = -100  # We only compute loss on masked tokens
+
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = (
+            torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+        )
+        inputs[indices_replaced] = self.tokenizer.convert_tokens_to_ids(
+            self.tokenizer.mask_token
+        )
+
+        # 10% of the time, we replace masked input tokens with random word
+        indices_random = (
+            torch.bernoulli(torch.full(labels.shape, 0.5)).bool()
+            & masked_indices
+            & ~indices_replaced
+        )
+        random_words = torch.randint(
+            len(self.tokenizer), labels.shape, dtype=torch.long
+        )
+        inputs[indices_random] = random_words[indices_random]
+
+        # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+        return inputs, labels
+
+    def pick_instance(self, labels: List[str]) -> int:
+        selectable_instances_idx = [
+            idx for idx, _label in enumerate(labels) if _label != "Nil"
+        ]
+
+        if len(selectable_instances_idx) == 0:
+            return -1
+
+        instances_inverse_frequency = np.array(
+            [
+                self.sense_inverse_frequencies[labels[inst_idx]]
+                for inst_idx in selectable_instances_idx
+            ]
+        )
+        instances_probs = instances_inverse_frequency / np.sum(
+            instances_inverse_frequency
+        )
+
+        return np.random.choice(selectable_instances_idx, p=instances_probs)
+
+    def encode_and_apply_masking(
+        self,
+        sample: Dict[str, Any],
+        instance_idx: Optional[int] = None,
+        definition: Optional[str] = None,
+    ) -> Dict[str, Any]:
+        if instance_idx is not None:
+            final_indices = (
+                # [CLS]
+                [self.tokenizer.cls_token_id]
+                # sentence ids
+                + sample["input_ids"]
+                # [DEFINITION]
+                + [self.tokenizer.convert_tokens_to_ids(self.definition_special_token)]
+                # definition ids
+                + self._tokenize_definition(definition)
+                # [SEP]
+                + [self.tokenizer.sep_token_id]
+            )
+
+            final_indices = torch.tensor(final_indices, dtype=torch.long)
+
+            input_indices = torch.clone(final_indices)
+            defined_token_boundaries = [
+                x + 1
+                for x in sample["word2tokens"][instance_idx]
+            ]
+            input_indices[
+                defined_token_boundaries[0] : defined_token_boundaries[1]
+            ] = self.tokenizer.convert_tokens_to_ids(self.defined_special_token)
+
+        else:
+            input_indices = (
+                # [CLS]
+                [self.tokenizer.cls_token_id]
+                # sentence ids
+                + sample["input_ids"]
+                # [SEP]
+                + [self.tokenizer.sep_token_id]
+            )
+            input_indices = torch.tensor(input_indices, dtype=torch.long)
+            final_indices = input_indices.clone()
+
+        return {
+            "input_indices": input_indices,
+            "final_indices": final_indices,
+        }
+
+    def process_samples(self, samples: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        processed_samples = []
+
+        if self.plain_mlm_probability > 0:
+            is_plain_mlm = torch.bernoulli(
+                torch.full((len(samples),), self.plain_mlm_probability)
+            ).bool()
+        else:
+            is_plain_mlm = [False] * len(samples)
+
+        for plain_mlm, sample in zip(is_plain_mlm, samples):
+
+            if plain_mlm:
+                encoding_output = self.encode_and_apply_masking(sample)
+            else:
+                chosen_instance = self.pick_instance(sample["labels"])
+                if chosen_instance < 0:
+                    continue
+
+                instance_definition = self.inventories[sample["dataset_id"]][sample["labels"][chosen_instance]]
+                encoding_output = self.encode_and_apply_masking(sample, chosen_instance, instance_definition)
+
+            processed_samples.append(encoding_output)
+
+        return processed_samples
+
+    def collate_function(self, samples: List[Dict[str, Any]]) -> Dict[str, Any]:
+
+        processed_samples = self.process_samples(samples)
+
+        input_indices = batchify(
+            [ps["input_indices"] for ps in processed_samples],
+            padding_value=self.tokenizer.pad_token_id,
+        )
+
+        attention_mask = batchify(
+            [torch.ones_like(ps["input_indices"]) for ps in processed_samples],
+            padding_value=0,
+        )
+
+        final_indices = batchify(
+            [ps["final_indices"] for ps in processed_samples],
+            padding_value=self.tokenizer.pad_token_id,
+        )
+
+        input_ids, labels = self._torch_mask_tokens(input_indices, final_indices)
+        labels[labels == self.tokenizer.pad_token_id] = -100
+
+        return dict(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            labels=labels,
+            )
+
+
 def main():
 
     oxford_inventory = SenseInventory(
         "/home/edobobo/PycharmProjects/dmlm/data/inventories/oxf.tsv"
     )
 
-    dmlm_dataset = DMLMDataset(
-        {"oxford": ["/home/edobobo/PycharmProjects/dmlm/data/datasets/oxford/oxf.0"]},
+    dmlm_dataset = EfficientDMLMDataset(
+        "data/processed_datasets/oxf_10.jsonl",
         {"oxford": oxford_inventory},
         "bert-base-cased",
         "[DEF]",
         "[DEFINITION]",
         0.15,
+        plain_mlm_probability=0.5,
     )
+
+    from torch.utils.data import DataLoader
+
+    dataloader = DataLoader(
+        dmlm_dataset, batch_size=16, collate_fn=dmlm_dataset.collate_function
+    )
+
+    for batch in dataloader:
+        print(batch)
 
     senses_counter = Counter()
     for sample in dmlm_dataset:
---
+++ src/scripts/covnert_raganato_to_jsonl.py
@@ -0,0 +1,67 @@
+import argparse
+import json
+from typing import List
+
+from tqdm import tqdm
+
+from src.utils.wsd import read_from_raganato, expand_raganato_path
+
+
+def convert_raganato_to_jsonl(
+    raganato_paths: List[str], identifiers: List[str], output_path: str
+) -> None:
+    with open(output_path, "w") as f:
+
+        for dataset_id, raganato_path in tqdm(
+            zip(identifiers, raganato_paths), desc="Iterating over datasets"
+        ):
+
+            for _, _, wsd_sentence in tqdm(
+                read_from_raganato(*expand_raganato_path(raganato_path)),
+                desc="Iterating over instances",
+            ):
+
+                tokens = [wi.annotated_token.text for wi in wsd_sentence]
+                labels = [
+                    wi.labels[0] if wi.labels is not None else None
+                    for wi in wsd_sentence
+                ]
+
+                # if len(tokens) < 5 or len(labels) < 5:
+                #     continue
+
+                if "\n" in tokens:
+                    print("ciao")
+
+                if len(tokens) != len(labels):
+                    continue
+
+                if any(x is None for x in tokens):
+                    continue
+
+                json_dump = {
+                    "sentence": " ".join(tokens),
+                    "labels": [l if l is not None else "Nil" for l in labels],
+                    "dataset_id": dataset_id,
+                }
+
+                f.write(json.dumps(json_dump))
+                f.write("\n")
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--raganato-paths", required=True, nargs="+")
+    parser.add_argument("--identifiers", required=True, nargs="+")
+    parser.add_argument("--output-path", required=True)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    assert len(args.raganato_paths) == len(args.identifiers)
+    convert_raganato_to_jsonl(args.raganato_paths, args.identifiers, args.output_path)
+
+
+if __name__ == "__main__":
+    main()
